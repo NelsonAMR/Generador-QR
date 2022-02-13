12c691a5be09ec2d09eadd841ccf46a6230d4745
NelsonAMR <nelson.macias99@gmail.com>
2022-02-13 01:18:59 -0600

    Generador QR v1.1

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ btn.addEventListener('click', () => {
     let data = input.value;
     let url = `https://api.qrserver.com/v1/create-qr-code/?&data=${data}`
     console.log(url);
-    if(data != '') qr.src = url;
+    if(data != '' && typeof(data) == 'string') qr.src = url;
+    input.value = '';
     // exito.classList.remove('ban');
 });
